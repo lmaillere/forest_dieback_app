9499d1b8717110713c56cfe90e7451fae7e1b2b6
Ludovic Mailleret <ludovic.mailleret@inrae.fr>
2022-09-06 14:46:36 +0200

    update defo, ombre

--- forest_dieback_app.py
+++ forest_dieback_app.py
@@ -18,7 +18,7 @@ with col12:
     st.markdown("### D'après le modèle de [Ritchie *et al.* 2021](https://www.nature.com/articles/s41586-021-03263-2)")
     st.markdown(" - $v$ est la proportion de végétation dans l'environnement")
     st.markdown("- $g(.)$ le taux de croissance de la végétation")
-    st.markdown("- $\gamma$ un taux de perturbation")
+    st.markdown("- $\gamma$ un taux de déforestation")
     st.markdown("Le modèle s'écrit :")
     st.markdown("$$ \dot v = g(.) v (1-v) - \gamma v $$")
     st.markdown("#")
@@ -30,22 +30,22 @@ with col13:
     st.markdown(r"$$ g(T) = g_0 \left[1-\left(\frac{T_{opt}-T}{\beta}\right)^2\right]$$")
     st.markdown("Par ailleurs, la température locale $T$ décroît avec la végétation :")
     st.markdown("$$ T = T_f + a (1-v) $$")
-    st.markdown(r"Les paramètres $a$ et $\beta$ caractérisent la sensibilité de $g(.)$ et $T$ à la température locale et à la végétation, respectivement. $T_f$, la température de forçage est la température de l'environnement lorsqu'il est recouvert de forêts")
+    st.markdown(r"Les paramètres $a$ et $\beta$ caractérisent la sensibilité de $g(.)$ et $T$ à la température locale et à la végétation, respectivement. $T_f$, la température de l'environnement à l'ombre.")
     st.markdown("##")
 
 with col12:
     st.markdown("### Calculs et simulations")
     st.markdown("Saisissez les paramètres")
-    
+
 col21, col22, col23, col24 = st.columns([3, 7, 7, 7], gap = "large")
 with col22:
     v0 = st.slider(' Proportion de végétation initiale', min_value=0., max_value=1., value = .3, step=0.05)  
 
 with col23:
-    gamma = st.slider(' Taux de perturbation', min_value=.1, max_value=.7, value = .3, step=0.05) 
+    gamma = st.slider(' Taux de déforestation', min_value=.1, max_value=.7, value = .3, step=0.05) 
 
 with col24:
-    T_f =  st.slider(' Température de forçage', min_value=16., max_value=30., value = 22., step=0.5)  
+    T_f =  st.slider(' Température de l\'environnement à l\'ombre', min_value=16., max_value=30., value = 22., step=0.5)  
 
 # encapsulation
 params_sim = np.array([g_0, T_opt, beta, T_f, a, gamma])
